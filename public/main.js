1117eacc95f96cf4b58a6aa60df5c713da1b107f
Vivek Khimani <vivekkhimani07@gmail.com>
2020-03-16 03:16:47 -0400

    machine learning model deployed on client. fully functional now. wrote an alternative code for displaying tasks which includes live timer. done on my end. Brady and Evelynn needs to give final design changes. The backend is complete

--- public/main.js
+++ public/main.js
@@ -2,10 +2,17 @@ window.onload = function() {
     document.getElementById("signoutBtn").addEventListener("click", signoutUser);
     document.getElementById("showRemoveBtn").addEventListener("click", showRemoveBtn);
     document.getElementById("removeBtn").addEventListener("click", removeUser);
-    document.getElementById("addTaskBtn").addEventListener("click", addTask)
+    document.getElementById("addTaskBtn").addEventListener("click", addTask);
     requestData();
     requestTasks();
-}
+    if (sessionStorage.hist == null) {
+        getHistoryML();
+    }
+    else{
+        console.log(sessionStorage.hist);
+        displayHistory(sessionStorage.hist);
+    }
+};
 
 function addTask() {
     var task = document.getElementById("task").value;
@@ -42,6 +49,8 @@ function addTask() {
             console.log(err);
         }
     });
+    sessionStorage.removeItem('hist');
+    getHistoryML();
 }
 
 function signoutUser(event) {
@@ -95,6 +104,10 @@ function requestTasks() {
             //let userInfo = json[0];
             console.log(json);
             formatTasks(json);
+            //call for vivek's alternative display
+            alternative(json);
+            //interval that updates timer.
+            setInterval(updateTimer.bind(null,json),5000);
         }
     };
 
@@ -106,6 +119,120 @@ function requestTasks() {
     xhttp.send();
 }
 
+//produces output that can be used in the timer
+function produceTimer(dueDate){
+    //return - [distance,days,hours,minutes,seconds]
+    var dueTime = dueDate.getTime();
+    var nowTime = new Date().getTime();
+
+    //check if the due date < curr Date
+    var distance = dueTime - nowTime;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+    return [distance,days,hours,minutes];
+}
+
+//alternative display method with timer for tasks
+function alternative(taskJson){
+    var taskDiv = document.getElementById("alternative_tasks");
+
+    const h3_tag = document.createElement("h3");
+    h3_tag.appendChild(document.createTextNode("Here's an alternative display of todo list"));
+    taskDiv.appendChild(h3_tag);
+
+    const br1_init = document.createElement("br");
+    taskDiv.appendChild(br1_init);
+
+    const display_table = document.createElement("table");
+    const header_row = display_table.insertRow(0);
+
+    const cell1 = header_row.insertCell(0); //completed check_box
+    const cell2 = header_row.insertCell(1); //task
+    const cell3 = header_row.insertCell(2); //priority
+    const cell4 = header_row.insertCell(3); //due_date
+    const cell5 = header_row.insertCell(4); //time
+    const cell6 = header_row.insertCell(5); //timer
+
+    cell1.innerHTML = "<b>Completed</b>";
+    cell2.innerHTML = "<b>Task</b>";
+    cell3.innerHTML = "<b>Priority</b>";
+    cell4.innerHTML = "<b>Due Date</b>";
+    cell5.innerHTML = "<b>Due Time</b>";
+    cell6.innerHTML = "<b>Timer</b>";
+
+    var row = 1;
+    var td_count;
+    var data_row,dc;
+    for (i=0;i<taskJson.length;i++){
+        td_count = 0;
+        data_row = display_table.insertRow(row);
+        while(td_count<6){
+            dc = data_row.insertCell(td_count);
+            if (td_count==0){
+                var id_val = "check_input"+i;
+                dc.innerHTML = "<input type='checkbox' id='"+id_val+"'>";
+            }
+            else if(td_count==1){
+                dc.innerHTML = taskJson[i].task;
+            }
+            else if(td_count==2){
+                dc.innerHTML = taskJson[i].priority;
+            }
+            else if(td_count==3){
+                var date_time_list = taskJson[i].due_date.split("T");
+                dc.innerHTML = date_time_list[0];
+            }
+            else if(td_count==4){
+                var date_time_list1 = taskJson[i].due_date.split("T");
+                dc.innerHTML = date_time_list1[1];
+            }
+            else if(td_count==5){
+                var timer_id = "timer"+i;
+                console.log(timer_id);
+                dc.innerHTML = "<p id='"+timer_id+"'>";
+            }
+            td_count++;
+        }
+        row++;
+        taskDiv.appendChild(display_table);
+        taskDiv.appendChild(br1_init);
+    }
+}
+
+//function to call for timer activation every second
+function updateTimer(taskJson){
+    var expired = [];
+    for (i=0;i<taskJson.length;i++){
+        //ignore if the task has expired
+        if (expired.includes(i)){
+            continue;
+        }
+
+        const dueDate = new Date(taskJson[i].due_date);
+        var produce = produceTimer(dueDate);
+        var timer_id = "timer"+i;
+
+        //if expired
+        if (produce[0] < 0){
+            expired.push(i);
+            document.getElementById(timer_id).innerHTML = "Overdue";
+            document.getElementById(timer_id).style.color = "red";
+            continue;
+        }
+        document.getElementById(timer_id).innerHTML = produce[1].toString() + "d " + produce[2].toString() + "h "+ produce[3].toString() + "m";
+        if (produce[1]>3){
+            document.getElementById(timer_id).style.color = "green";
+        }
+        else if (produce[1]<3 && produce[1]>1){
+            document.getElementById(timer_id).style.color = "orange";
+        }
+        else if (produce[1] < 1){
+            document.getElementById(timer_id).style.color = "red";
+        }
+    }
+}
 
 function formatTasks(taskJson) {
     taskDiv = document.getElementById("taskDiv");
@@ -143,7 +270,9 @@ function formatTasks(taskJson) {
 
         //Formatting the date
         date = new Date(taskJson[i].due_date);
-        formatDate = (date.getMonth()+1) + '-'+date.getDate() + "-" + date.getFullYear();
+
+        formatDate = (date.getMonth()+1) + '-' + date.getDate() + "-" + date.getFullYear();
+
 
         //Formatting the time
         var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
@@ -227,7 +356,7 @@ function showRemoveBtn(event) {
 function removeUser(event) {
     var send_data = {};
     send_data.email = document.getElementById("email").value;
-    console.log(send_data.email)
+    console.log(send_data.email);
 
     $.ajax({
         url: 'http://localhost:3000/remove_db',
@@ -244,3 +373,79 @@ function removeUser(event) {
         }
     });
 }
+
+//a function that triggers machine learning algorithm in the backend only when cache is empty or a new task is added.
+async function getHistoryML(){
+    await $.ajax({
+        url: 'http://localhost:3000/get_custom_recommendations',
+        type: 'GET',
+        success: async function(resp,msg){
+            await resp;
+            sessionStorage.setItem("hist",JSON.stringify(resp));
+            if (sessionStorage.hist){
+                location.reload();
+                //console.log(sessionStorage.hist);
+            }
+
+        },
+        error: function(jqXHR,status,err){
+            console.log(status);
+            console.log(err);
+        }
+    });
+}
+
+//a function that displays the history
+function displayHistory(histObject){
+    //sort data by maximum and displaying top 10
+    const histArray = JSON.parse(histObject);
+    console.log(histArray);
+    histArray.sort(function(a,b){
+        return b.similarity-a.similarity;
+    });
+    var historyDiv = document.getElementById("history_div");
+    historyDiv.style.visibility = "visible";
+    var display_table = document.createElement("TABLE");
+    var header_row = display_table.insertRow(0);
+
+    //populate the first row
+    var cell1 = header_row.insertCell(0);
+    var cell2 = header_row.insertCell(1);
+    var cell3 = header_row.insertCell(2);
+
+    cell1.innerHTML = "<b>Recommended Task</b>";
+    cell2.innerHTML = "<b>Base Task</b>";
+    cell3.innerHTML = "<b>Similarity (%)</b>";
+
+    //display top 10 similar tasks
+    var i = 0;
+    var row = 1;
+    var td_count;
+    var data_row,dc;
+    while(i<10){
+        td_count = 0;
+        data_row = display_table.insertRow(row);
+        while(td_count<3){
+            dc = data_row.insertCell(td_count);
+            if (td_count == 0){
+                //recommended
+                dc.innerHTML = histArray[i].recommended_task;
+            }
+            else if (td_count == 1){
+                //user task
+                dc.innerHTML = histArray[i].user_task;
+            }
+            else if (td_count == 2){
+                //similarity
+                dc.innerHTML = histArray[i].similarity;
+            }
+            td_count++;
+            continue;
+        }
+        i++;
+        row++;
+    }
+    historyDiv.appendChild(display_table);
+}
+
+
